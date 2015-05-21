735fc8b05e81decd1cc019b8a14a9c981ac8e3ef
Leo Baran <leobaran@cs.washington.edu>
2015-05-20 18:25:30 -0700

    able to determine presence of a dot.

--- characterRecognitionFiles/dotRecognition/dataFileGenerator.sh
+++ characterRecognitionFiles/dotRecognition/dataFileGenerator.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+#Input: a file containing 3 by 3 blocks of 1s and 0s followed by
+#one line containing either a 1 or a 0. The 3x3 blocks represent the input
+#data to the neural network and the boolean after them is the target output
+#that specifies whether the block contains a 2x2 dot.
+
+#sample input:
+#110
+#110
+#000
+#1
+
 filename="$1"
 
 touch dataInput.txt
+rm dataInput.txt
+touch dataInput.txt
 
-echo "9 9 7 1" >> dataInput.txt
-echo "20" >> dataInput.txt
+echo "9 9 9 1" >> dataInput.txt
+echo "60" >> dataInput.txt
 
 let index=1
 
